dab27105a580c901e33b420078c2af74beccaed9
Rob Berger <robertberger5@gmail.com>
2020-04-22 17:30:29 -0500

    changes name from "tabIt" to "tabby"

--- DocumentRoot/test.php
+++ DocumentRoot/test.php
@@ -1,5 +1,5 @@
 <?php
-$pdo = new PDO('mysql:host=db;dbname=tabit;charset=utf8', 'tab_admin', 'controllerofthetabs');
+$pdo = new PDO('mysql:host=db;dbname=tabby;charset=utf8', 'tab_admin', 'controllerofthetabs');
 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 $stmt=$pdo->query('SELECT * FROM tabs');
