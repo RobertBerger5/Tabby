9b5c4c4846fdf905818cb3674042bd5b0bf5e7e9
Rob Berger <robertberger5@gmail.com>
2020-04-29 12:59:59 -0500

    automatically initialize database on build

---
+++ init_db.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS users (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	username VARCHAR(255) NOT NULL UNIQUE,
+	pass VARCHAR(255) NOT NULL
+) ENGINE=INNODB;
+
+CREATE TABLE IF NOT EXISTS tabs (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	title VARCHAR(255) DEFAULT '(untitled)',
+	user INT NOT NULL, /*user id of tab owner*/
+	forked_from INT NOT NULL, /*tab id of where it was forked from*/
+	is_public BOOLEAN NOT NULL,
+	last_edit TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+	/*there is a JSON type in MySQL, but we should never have to interact with it other than updating the entire .json file*/
+	tab_data MEDIUMTEXT NOT NULL,
+	/*commented out because why not let users have multiple tabs with the same name? Maybe ask them if they're sure they want to save under the same name as an existing tab first*/
+	/*CONSTRAINT no_same_titles UNIQUE (title,user),*/
+	FOREIGN KEY (user) REFERENCES users(id),
+	FOREIGN KEY (forked_from) REFERENCES tabs(id)
+) ENGINE=INNODB;
+
+/*one tab being shared with a user*/
+CREATE TABLE IF NOT EXISTS shares (
+	tab INT NOT NULL,
+	user INT NOT NULL,
+	can_edit BOOLEAN NOT NULL,
+	CONSTRAINT no_multiple_share UNIQUE (tab, user),
+	FOREIGN KEY (tab) REFERENCES tabs(id),
+	FOREIGN KEY (user) REFERENCES users(id)
+) ENGINE=INNODB;
+
+/*TODO: run my password through the same encryption as the final product will use, and insert that straight into here*/
+INSERT INTO users VALUES (1,'rob','temporarypassword');
+
+INSERT INTO tabs VALUES (1,'(blank)',1,1,TRUE,CURRENT_TIMESTAMP,'TODO: insert the most basic tab json here bruh');
+/*Note: this tab says it's forked from itself because "forked from" can't be null. Might have to allow null just for this, I still want everyone to fork from one original tab*/
