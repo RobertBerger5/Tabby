7dceb3a20a8ea8e6c7816bf36d03210505f8fc2a
Rob Berger <robertberger5@gmail.com>
2020-06-09 20:07:56 -0500

    fix dumb issue with $id instead of $tab_id

--- DocumentRoot/ajaxFiles/saveTab.php
+++ DocumentRoot/ajaxFiles/saveTab.php
@@ -2,20 +2,19 @@
 //will die if user doesn't have permission to view, or any other issue
 require 'ajaxCheck.php';
 
-if(!$can_edit){
+if($can_edit){
 	die("E: No edit priviledge");
 }
 
 //TODO: check for validity? Don't want them saving weird things as tabs
 //also potential vulnerability with shared users opening up whatever weird JSON someone else sent there??
-
 try{
-	$res=querySafe("UPDATE tabs SET tab_data=? WHERE tabs.id=?",[$_POST["data"],$id]);
-	die($res);
+	$res=querySafe("UPDATE tabs SET tab_data=? WHERE tabs.id=?",[$_POST["data"],$tab_id]);
 }catch(Exception $e){
-	echo "PDO ERROR: ".$e->getMessage()."<br>";
 	http_response_code(500);
-	die("PDO Error");
+	die("PDO Error ".$e->getMessage());
 }
-die("Updated");
+
+http_response_code(200);
+die("Tab Saved");
 ?>
